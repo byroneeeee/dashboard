cfe5018d4b7d3752065a4d84aaef94f563265f49
byroneeeee <119042363+byroneeeee@users.noreply.github.com>
2023-03-18 21:24:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        
+        <title> Sidebar</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+            <!-- Sidebar-->
+            <div class="border-end bg-white" id="sidebar-wrapper">
+                <div class="sidebar-heading border-bottom bg-light">menu</div>
+                <div class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Employee details</a>             
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+                </div>
+            </div>
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <div class="container-fluid">
+                        <button class="btn btn-primary" id="sidebarToggle"> Menu</button>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                               
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- Page content-->
+                <style>
+                    .wrapper{
+                        width: 600px;
+                        margin: 0 auto;
+                    }
+                    table tr td:last-child{
+                        width: 120px;
+                    }
+                </style>
+                <script>
+                    $(document).ready(function(){
+                        $('[data-toggle="tooltip"]').tooltip();   
+                    });
+                </script>
+            </head>
+            <body>
+                <div class="wrapper">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="mt-5 mb-3 clearfix">
+                                    <h2 class="pull-left">Employee Details</h2>
+                                    <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
+                                </div>
+                                <?php
+                                // Include config file
+                                require_once "config.php";
+                                
+                                // Attempt select query execution
+                                $sql = "SELECT * FROM demo";
+                                if($result = mysqli_query($link, $sql)){
+                                    if(mysqli_num_rows($result) > 0){
+                                        echo '<table class="table table-bordered table-striped">';
+                                            echo "<thead>";
+                                                echo "<tr>";
+                                                    echo "<th>#</th>";
+                                                    echo "<th>Name</th>";
+                                                    echo "<th>Address</th>";
+                                                    echo "<th>Salary</th>";
+                                                    echo "<th>Action</th>";
+                                                echo "</tr>";
+                                            echo "</thead>";
+                                            echo "<tbody>";
+                                            while($row = mysqli_fetch_array($result)){
+                                                echo "<tr>";
+                                                    echo "<td>" . $row['id'] . "</td>";
+                                                    echo "<td>" . $row['name'] . "</td>";
+                                                    echo "<td>" . $row['address'] . "</td>";
+                                                    echo "<td>" . $row['salary'] . "</td>";
+                                                    echo "<td>";
+                                                    echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                                    echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                                    echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                                echo "</tr>";
+                                            }
+                                            echo "</tbody>";                            
+                                        echo "</table>";
+                                        // Free result set
+                                        mysqli_free_result($result);
+                                    } else{
+                                        echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                                    }
+                                } else{
+                                    echo "Oops! Something went wrong. Please try again later.";
+                                }
+             
+                                // Close connection
+                                mysqli_close($link);
+                                ?>
+                            </div>
+                        </div>        
+                    </div>
+                </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
